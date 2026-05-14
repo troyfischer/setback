85a5a30692adfe4b7808fd649f800f05474ca6a5
troyfischer <troytfischer@gmail.com>
2026-05-13 20:08:51 -0500

    feat: cors config

--- python/src/main.py
+++ python/src/main.py
@@ -69,7 +69,7 @@ app = FastAPI(
 settings = Settings()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=settings.cors_origins,
+    allow_origin_regex=settings.cors_origin_regex,
     allow_credentials=True,
     allow_headers=["*"],
     allow_methods=["*"],
